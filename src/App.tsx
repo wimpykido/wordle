b55e40341826216305734381ff79a56d78673d6f
eliso <eliso.saladze5@gmail.com>
2023-12-05 20:13:38 +0400

    small mistake fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/sign-in" element={<WordlePage />} />
+        <Route path="/" element={<WordlePage />} />
       </Routes>
     </BrowserRouter>
   );
