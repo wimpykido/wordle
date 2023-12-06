7ca68cdd7ae99ef27aa655187aeb1bd4cafe7a96
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-12-06 22:34:27 +0400

    Added keyboard

--- src/components/keyboard/index.tsx
+++ src/components/keyboard/index.tsx
@@ -1,5 +1,41 @@
 //KeyboardButton componentis sashualebit keyboardi
+import { useState } from "react";
+import { KeyboardButton } from "../keyboard-button";
+import keys from "./keys";
 
 export const Keyboard = () => {
-    return <div>this should be keyboard</div>
+  const [shift, setShift] = useState<boolean>(false);
+  const shiftValue = (value: string) => {
+    switch (value) {
+      case "წ":
+        return "ჭ";
+      case "რ":
+        return "ღ";
+      case "ტ":
+        return "თ";
+      case "ს":
+        return "შ";
+      case "ჯ":
+        return "ჟ";
+      case "ზ":
+        return "ძ";
+      case "ც":
+        return "ჩ";
+      default:
+        return value;
+    }
+  };
+
+  return (
+    <div className="grid grid-rows-3 grid-cols-10 max-w-[38rem] m-1 gap-1 sm:gap-2">
+      {keys.map((value, i) => (
+        <KeyboardButton
+          key={i}
+          value={shift ? shiftValue(value) : value}
+          shift={shift}
+          setShift={setShift}
+        />
+      ))}
+    </div>
+  );
 };
--- src/pages/wordle-page/index.tsx
+++ src/pages/wordle-page/index.tsx
@@ -1,5 +1,12 @@
+import { Keyboard } from "../../components/keyboard";
+
 const WordlePage = () => {
-  return <div>this is game page</div>;
+  return (
+    <div>
+      <div>this is game page</div>
+      <Keyboard />
+    </div>
+  );
 };
 
 export default WordlePage;
