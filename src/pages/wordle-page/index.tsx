d5aabe3d611d02b09ed3f90d9b7d8cf7c6154123
eliso <eliso.saladze5@gmail.com>
2023-12-07 22:36:25 +0400

    changes

--- src/pages/wordle-page/index.tsx
+++ src/pages/wordle-page/index.tsx
@@ -3,14 +3,12 @@ import { Word } from "../../components/word";
 
 const WordlePage = () => {
   return (
-    (
     <div>
       <div className="grid gap-2">
-      {Array.from({ length: 6 }).map((_, index) => (
-        <Word length={6} />
-      ))}
-    </div>
-  )
+        {Array.from({ length: 6 }).map((_, index) => (
+          <Word length={6} />
+        ))}
+      </div>
       <Keyboard />
     </div>
   );
