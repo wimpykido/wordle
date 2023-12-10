158380d2e9579fe04e9ee9e2032129d9fd535e85
eliso <eliso.saladze5@gmail.com>
2023-12-10 17:39:28 +0400

    changed letterInput markup, implemented logic to control moving to the next input

--- src/components/letter-input/index.tsx
+++ src/components/letter-input/index.tsx
@@ -1,15 +1,55 @@
+import { useEffect, useRef, useState } from "react";
+
 type LetterInputProps = {
   letter?: string;
+  onUserInput: (input: string, moveNext: () => void) => void;
+  disabled: boolean;
+  autoFocus?: boolean;
 };
-export const LetterInput = ({ letter }: LetterInputProps) => {
+export const LetterInput = ({
+  letter,
+  onUserInput,
+  disabled,
+  autoFocus,
+}: LetterInputProps) => {
+  const [input, setInput] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const userInput = event.target.value;
+    setInput(userInput);
+  };
+  const handleMoveNext = () => {
+    if (!disabled) {
+      onUserInput(input, () => {
+        if (inputRef.current) {
+          inputRef.current.blur();
+        }
+      });
+    }
+  };
+  useEffect(() => {
+    // Focus on the input when it becomes the selected input or autoFocus is true
+    if ((inputRef.current && !disabled) || autoFocus) {
+      inputRef.current?.focus();
+    }
+  }, [input, disabled, autoFocus]);
+  useEffect(() => {
+    if (input && !disabled) {
+      handleMoveNext();
+    }
+  }, [disabled, input]);
   return (
     <div
-      className={`border-2 flex items-center justify-center bg-customBackground rounded-md w-14 h-14
-        ${letter ? "border-customBorder2" : "border-customBorder"}`}
+      className={`border-2 flex items-center justify-center bg-customBackground rounded-md w-14 h-14`}
     >
-      <p className="text-35 text-center text-letterColor font-semibold">
-        {letter ? letter : null}
-      </p>
+      <input
+        type="text"
+        className={`text-35 text-center text-letterColor font-semibold uppercase border-none outline-none w-full h-full`}
+        value={letter || input}
+        onChange={handleChange}
+        maxLength={1} // Limit input to one character
+        disabled={disabled}
+      />
     </div>
   );
 };
--- src/pages/wordle-page/index.tsx
+++ src/pages/wordle-page/index.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { Keyboard } from "../../components/keyboard";
 import { Word } from "../../components/word";
 
 const WordlePage = () => {
+  const [wordStates, setWordStates] = useState(
+    Array(6).fill("").map(() => ({ guessedWord: Array(6).fill("") }))
+  );
+  const [selectedInput, setSelectedInput] = useState<number>(0);
+  const handleUserInput = (index: number, input: string, moveNext: () => void) => {
+    setWordStates((prevWordStates) => {
+      const updatedStates = [...prevWordStates];
+      updatedStates[index].guessedWord[selectedInput] = input;
+      return updatedStates;
+    });
+    moveNext();
+  };
+  console.log(wordStates)
   return (
-    <div>
+    <div className="grid items-center justify-center">
       <div className="grid gap-2">
-        {Array.from({ length: 6 }).map((_, index) => (
-          <Word length={6} />
+        {wordStates.map((wordState, index) => (
+          <Word
+            key={index}
+            secretWord={"კარადა".split("")}
+            word={wordState.guessedWord}
+            onUserInput={(input) => {
+              handleUserInput(index, input, () => {
+                // Move to the next input if not at the end
+                if (selectedInput < wordState.guessedWord.length - 1) {
+                  setSelectedInput((prevSelectedInput) => prevSelectedInput + 1);
+                }
+              });
+            }}
+          />
         ))}
       </div>
       <Keyboard />
