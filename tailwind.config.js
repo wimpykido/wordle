415d96f9b3bb2b7eedb27a4ad8b56834bb02f1e2
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-12-26 00:40:20 +0400

    Theme fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {},
     extend: {
-      custom: {
-        light: "#dce1ed",
-        dark: "#a4aec4",
-        yellow: "#f3c237",
-        green: "#79b851",
-        text: "#414a5e",
-        border: "#dee1e9",
-        borderDark: "#a7adc0",
-        hover: "#c4cbdd",
+      colors: {
+        custom: {
+          light: "#dce1ed",
+          dark: "#a4aec4",
+          yellow: "#f3c237",
+          green: "#79b851",
+          text: "#414a5e",
+          border: "#dee1e9",
+          borderDark: "#a7adc0",
+          hover: "#c4cbdd",
+        },
       },
     },
   },
