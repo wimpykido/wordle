bb00eaa4e744eb64c9bfb210472144e57fd5cf27
Eliso Saladze <eliso.saladze5@gmail.com>
2023-12-26 20:47:14 +0400

    color changing func on custom keyboard

--- src/components/keyboard/keys.ts
+++ src/components/keyboard/keys.ts
@@ -1,33 +1,77 @@
-let keys: string[];
-// eslint-disable-next-line
-export default keys = [
-  "ქ",
-  "წ",
-  "ე",
-  "რ",
-  "ტ",
-  "ყ",
-  "უ",
-  "ი",
-  "ო",
-  "პ",
-  "ა",
-  "ს",
-  "დ",
-  "ფ",
-  "გ",
-  "ჰ",
-  "ჯ",
-  "კ",
-  "ლ",
-  "del",
-  "shift",
-  "ზ",
-  "ხ",
-  "ც",
-  "ვ",
-  "ბ",
-  "ნ",
-  "მ",
-  "Enter",
+// let keys: string[];
+// // eslint-disable-next-line
+// export default keys = [
+//   "ქ",
+//   "წ",
+//   "ე",
+//   "რ",
+//   "ტ",
+//   "ყ",
+//   "უ",
+//   "ი",
+//   "ო",
+//   "პ",
+//   "ა",
+//   "ს",
+//   "დ",
+//   "ფ",
+//   "გ",
+//   "ჰ",
+//   "ჯ",
+//   "კ",
+//   "ლ",
+//   "del",
+//   "shift",
+//   "ზ",
+//   "ხ",
+//   "ც",
+//   "ვ",
+//   "ბ",
+//   "ნ",
+//   "მ",
+//   "Enter",
+// ];
+type letterType = {
+  letter: string;
+  color: string;
+};
+
+let keys: Array<letterType>;
+
+// ... (existing keys definition)
+
+const defaultColor = "bg-custom-light";
+
+keys = [
+  { letter: "ქ", color: defaultColor },
+  { letter: "წ", color: defaultColor },
+  { letter: "ე", color: defaultColor },
+  { letter: "რ", color: defaultColor },
+  { letter: "ტ", color: defaultColor },
+  { letter: "ყ", color: defaultColor },
+  { letter: "უ", color: defaultColor },
+  { letter: "ი", color: defaultColor },
+  { letter: "ო", color: defaultColor },
+  { letter: "პ", color: defaultColor },
+  { letter: "ა", color: defaultColor },
+  { letter: "ს", color: defaultColor },
+  { letter: "დ", color: defaultColor },
+  { letter: "ფ", color: defaultColor },
+  { letter: "გ", color: defaultColor },
+  { letter: "ჰ", color: defaultColor },
+  { letter: "ჯ", color: defaultColor },
+  { letter: "კ", color: defaultColor },
+  { letter: "ლ", color: defaultColor },
+  { letter: "del", color: defaultColor },
+  { letter: "shift", color: defaultColor },
+  { letter: "ზ", color: defaultColor },
+  { letter: "ხ", color: defaultColor },
+  { letter: "ც", color: defaultColor },
+  { letter: "ვ", color: defaultColor },
+  { letter: "ბ", color: defaultColor },
+  { letter: "ნ", color: defaultColor },
+  { letter: "მ", color: defaultColor },
+  { letter: "Enter", color: defaultColor },
 ];
+
+export default keys;
