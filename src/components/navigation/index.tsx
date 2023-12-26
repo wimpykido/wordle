cef19866b2c5b29e8281c4efec82f6f915300989
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-12-26 23:17:52 +0400

    Added navbar

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,3 +1,35 @@
+import StatsIcon from "../../assets/stats.png";
+import SettingsIcon from "../../assets/settings.png";
+import HelpIcon from "../../assets/help.png";
+
 export const Navigation = () => {
-    return <div>Navigation</div>
+  return (
+    <nav className="flex justify-between mt-2">
+      <button
+        className={`font-custom rounded-md outline-none text-custom-text hover:bg-custom-light transition-all sm:text-lg`}
+      >
+        <p className="px-2 pt-1 sm:pt-[6px] sm:px-4">დანებება</p>
+      </button>
+      <div className="flex">
+        <button
+          className={`font-custom rounded-md outline-none ml-2 p-2 text-custom-text hover:bg-custom-light transition-all sm:text-lg`}
+          title="coming soon..."
+        >
+          <img src={StatsIcon} className="w-5 h-5 sm:w-6 sm:h-6" />
+        </button>
+        <button
+          className={`font-custom rounded-md outline-none ml-2 p-2 text-custom-text hover:bg-custom-light transition-all sm:text-lg`}
+          title="coming soon..."
+        >
+          <img src={SettingsIcon} className="w-5 h-5 sm:w-6 sm:h-6" />
+        </button>
+        <button
+          className={`font-custom rounded-md outline-none ml-2 p-2 text-custom-text hover:bg-custom-light transition-all sm:text-lg`}
+          title="coming soon..."
+        >
+          <img src={HelpIcon} className="w-5 h-5 sm:w-6 sm:h-6" />
+        </button>
+      </div>
+    </nav>
+  );
 };
