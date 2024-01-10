1b23844a46312a1d9a62071d443f8436f2f760bd
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-01-10 23:58:52 +0400

    css chages

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -11,10 +11,8 @@ export const Alert = () => {
     return () => clearTimeout(timeout);
   }, []);
   return showComponent ? (
-    <div className="absolute flex justify-center items-center w-screen h-screen z-20">
-      <div className="font-custom bg-white p-6 rounded-lg shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
-        <h2 className="pt-2">გამოიცანი სიტყვა!</h2>
-      </div>
+    <div className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-56 font-custom text-lg py-3 rounded-lg shadow-[rgba(0,_0,_0,_0.3)_0px_2px_30px]">
+      <h2 className="pt-[6px] text-center">გამოიცანი სიტყვა!</h2>
     </div>
   ) : (
     <></>
